8ff257e1fc333e9eea89d8424e7461d09e971609
Brian Lough <ecenuig@gmail.com>
2018-11-24 15:02:36 +0000

    Adding parse partcipants component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,19 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ParseParticipantsComponent } from './parse-participants/parse-participants.component';
+
+import { PapaParseModule } from 'ngx-papaparse';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ParseParticipantsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    PapaParseModule
   ],
   providers: [],
   bootstrap: [AppComponent]
